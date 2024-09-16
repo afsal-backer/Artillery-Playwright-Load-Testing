33e73b3320c45653057119b8c7ca44737263e470
Afsal_Veed <gm4afsal@gmail.com>
2024-09-16 19:55:07 +0100

    Add phases

--- artillery/cart/add-to-cart.runner.ts
+++ artillery/cart/add-to-cart.runner.ts
@@ -4,7 +4,10 @@ import { testAddToCart } from './add-to-cart.actions'
 
 
 export async function addToCartTest(page: Page, context: any) {
+  console.log("Load testing in progress")
   await page.goto(context.vars.target)
   await loginToSauceLabs(page)
   await testAddToCart(page, context)
 }
+
+
